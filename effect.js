624250b4af2187de2c608b59515829cc992ffcc2
Ulvin <44314608+UlikGames@users.noreply.github.com>
2020-06-23 16:48:08 +0400

    Add files via upload

--- effect.js
+++ effect.js
@@ -156,7 +156,7 @@ $('document').ready(function(){
 		$('#b44').animate({top:240, left: vw-50},500);
 		$('#b55').animate({top:240, left: vw+50},500);
 		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		// $('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
